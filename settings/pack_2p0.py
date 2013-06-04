93a6a2d41d402f57313425d98110c4665dd09ed3
Ben Yee <l3enny@gmail.com>
2013-06-04 09:41:55 -0400

    Added more settings files.

---
+++ settings/pack_2p0.py
@@ -0,0 +1,18 @@
+from xsections.helium.pack import sigma
+import distributions
+import numpy as np
+from constants import q
+
+temperatures = np.logspace(-1, np.log10(200)) * q
+
+# Define the distribution to use and any inputs besides the temperature
+distribution = distributions.drumax
+kargs = {'x':2.0}
+
+dump = 'pack_2p0'
+comments = {'Cross-Sections':'Pack, 1992',
+            'Distribution':'Generalized Maxwellian',
+            'x':kargs['x'],
+            'Temperatures':'0.1-200 eV, log spacing, 50 points',
+            'Author':'Ben Yee'}
+elastic = True
