fb60adb1342a0d46ae359818b2ec69f1bc41c5e0
Ben Yee <l3enny@gmail.com>
2013-06-11 15:47:37 -0400

    Increased sampling density and upper limit.

--- settings/ralchenko_2p0.py
+++ settings/ralchenko_2p0.py
@@ -4,7 +4,7 @@ import numpy as np
 from xsections.helium.states import states
 from constants import q
 
-temperatures = np.logspace(-1, 2) * q
+temperatures = np.logspace(-1, np.log10(300), 1e2) * q
 
 # Define the distribution to use and any inputs besides the temperature
 distribution = distributions.drumax
@@ -14,7 +14,7 @@ dump = 'ralchenko_2p0'
 comments = {'Cross-Sections':'Ralchenko, 2008',
             'Distribution':'Generalized Maxwellian',
             'x':kargs['x'],
-            'Temperatures':'0.1-100 eV, log spacing, 50 points',
+            'Temperatures':'0.1-300 eV, log spacing, 100 points',
             'Author':'Ben Yee'}
 
 elastic = False
