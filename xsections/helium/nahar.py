4ec4ca56781d073378334376551f3936a9e86ee6
Ben Yee <l3enny@gmail.com>
2013-07-01 15:04:33 -0400

    Update to include proper interpolation method.

--- xsections/helium/nahar.py
+++ xsections/helium/nahar.py
@@ -2,12 +2,13 @@ import re
 
 import numpy as np
 from scipy.constants import *
+from scipy.interpolate import interp1d
 
 # Notify user that the recombination coefficents are for Maxwellian temperatures
 print "***NOTE*** Using Maxwellian recombination rate coefficients!"
 
 # Skip to this part of the file to simplify parsing.
-start = 299
+start = 298
 
 # Regex patterns for the state
 pattern = re.compile(r"(\d)(\d{2})(\d)(\d{2})(\d{2})\.(\d{2})(\d)(\d)")
@@ -18,13 +19,16 @@ with open("he1.rrc.ls.txt", mode="r") as f:
 
 temperatures = []
 states = []
+sigmas = []
 
-for line in lines[start:start+13]:
+# Read in the temperatures for each reaction rate
+for line in lines[start:start+14]:
     items = line.split()
     for i in items:
         if "(K)" in i:
-            temperatures.append(i.split("(K)")[0])
+            temperatures.append(float(i.split("(K)")[0]))
 
+# Read in the reaction rates for each temperature
 for lindex in range(start + 13, len(lines)):
     items = lines[lindex].split()
     if items:
@@ -38,11 +42,15 @@ for lindex in range(start + 13, len(lines)):
         tmp = lines[lindex:lindex + 12]
         tmp = [i.split() for i in tmp]
         sigma = sum(tmp, [])[2:]
-        sigma = [1e-6 * float(i) for i in sigma]
+        sigma = np.array([float(i) for i in sigma]) * 1e-6
+        sigmas.append(sigma)
         lindex += 12
     lindex += 1
 
+# Provide method to access interpolation of values
 def rrc(state, temperature):
     state = '0' + str(state)
-    func = interp1d(temperatures, sigma[states.index(state)], kind='cubic')
+    if state == '0100':
+        state = '0000'
+    func = interp1d(temperatures, sigmas[states.index(state)], kind='cubic')
     return func(temperature)
